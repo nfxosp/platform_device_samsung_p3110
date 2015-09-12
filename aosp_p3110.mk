363bb0f22984ae0d729d6fe364d265bec3b58fe6
Andreas Blaesius <skate4life@gmx.de>
2015-09-12 14:05:52 +0200

    P3110: let's build AOSP

---
+++ aosp_p3110.mk
@@ -0,0 +1,37 @@
+#
+# Copyright 2013 The Android Open-Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Release name
+PRODUCT_RELEASE_NAME := p3110
+
+# Inherit some common Rom stuff
+$(call inherit-product, vendor/aosp/config/common.mk)
+
+$(call inherit-product, device/samsung/p3110/full_p3110.mk)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := p3110
+PRODUCT_NAME := aosp_p3110
+PRODUCT_BRAND := samsung
+PRODUCT_MODEL := GT-P3110
+PRODUCT_MANUFACTURER := samsung
+
+#Set build fingerprint / ID / Prduct Name ect.
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=espressowifixx \
+    TARGET_DEVICE=espressowifi \
+    BUILD_FINGERPRINT="samsung/espressowifixx/espressowifi:4.2.2/JDQ39/P3110XXDMH1:user/release-keys" \
+    PRIVATE_BUILD_DESC="espressowifixx-user 4.2.2 JDQ39 P3110XXDMH1 release-keys"
